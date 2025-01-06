ed9adb749eccda36193ee29761716c04dd1e0429
Tin Mai <maihuutinti@gmail.com>
2025-01-06 23:30:30 +0700

    update

--- problem4/index.ts
+++ problem4/index.ts
@@ -1,3 +1,6 @@
+const input = 5
+
+console.log("function basicSum")
 function basicSum(n: number): number {
     let sum = 0;
     for (let i = 1; i <= n; i++) {
@@ -5,4 +8,16 @@ function basicSum(n: number): number {
     }
     return sum
 }
-console.log(basicSum(4))
+console.log(basicSum(input))
+
+console.log("function recursionSum")
+function recursionSum(n: number): number {
+    if (n <= 1) return n
+    return n + recursionSum(n - 1)
+}
+console.log(basicSum(input))
+console.log("function formulaSum")
+function formulaSum(n: number): number {
+    return (n * (n + 1)) / 2;
+}
+console.log(formulaSum(input))
